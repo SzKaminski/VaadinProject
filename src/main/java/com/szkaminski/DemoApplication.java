557c56b102f2d0e59dfb40bf1a6a4520cc83ee8c
Szymon Kamiński <kamin.szymon@gmail.com>
2019-06-03 16:33:04 +0200

    runnerclass add

--- src/main/java/com/szkaminski/DemoApplication.java
+++ src/main/java/com/szkaminski/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.szkaminski;
 
 import com.szkaminski.backend.model.User;
+import com.szkaminski.backend.repositories.UserRepository;
 import com.szkaminski.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -14,7 +17,17 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
 
     }
+}
 
+@Component
+class DemoCommandLineRunner implements CommandLineRunner {
 
+    @Autowired
+    private UserService userService;
 
+    @Override
+    public void run(String... args) throws Exception {
+        userService.addUser(new User("test@email.com", "test", "test123", "608608608"));
+        System.out.println("Runner!");
+    }
 }
