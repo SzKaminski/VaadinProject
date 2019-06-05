ca4295628fecaf6b2c27dd3b6115b39568a7aeea
Szymon Kamiński <kamin.szymon@gmail.com>
2019-06-05 10:59:12 +0200

    security init

---
+++ src/main/java/com/szkaminski/backend/configurations/WebSecurityConfiguration.java
@@ -0,0 +1,73 @@
+package com.szkaminski.backend.configurations;
+
+import com.szkaminski.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    public static final int BCRYPT_STRENGTH = 14;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.authenticationProvider(authenticationProvider());
+
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/styles/**")
+                .antMatchers("/frontend/**")
+                .antMatchers("/VAADIN/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .requestCache().requestCache(new CustomRequestCache())
+                .and().authorizeRequests()
+                .antMatchers("/","/list").permitAll()
+                .anyRequest().authenticated()
+                .antMatchers("/#").hasRole("DRIVER")
+                .antMatchers("/anonymous*").anonymous()
+                .and().formLogin().loginPage("/login").permitAll().loginProcessingUrl("/login")
+                .failureUrl("/login").successForwardUrl("/driver/account")
+
+                .and().logout().logoutSuccessUrl("/")
+                .logoutUrl("/perform_logout")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(BCRYPT_STRENGTH);
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+}
--- src/main/java/com/szkaminski/backend/service/UserService.java
+++ src/main/java/com/szkaminski/backend/service/UserService.java
@@ -1,14 +1,19 @@
 package com.szkaminski.backend.service;
 
+import com.szkaminski.backend.model.CustomUserDetails;
 import com.szkaminski.backend.model.User;
 import com.szkaminski.backend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
@@ -27,4 +32,12 @@ public class UserService {
     }
 
 
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        Optional<User> optionalUser = Optional.ofNullable(userRepository.findByLogin(login));
+        optionalUser
+                .orElseThrow(() -> new UsernameNotFoundException("login not found"));
+        return optionalUser
+                .map(CustomUserDetails::new).get();
+    }
 }
