6b1c74b8f7b57b4245fa8c24d7a8c4cc3fd089cb
Szymon Kamiński <kamin.szymon@gmail.com>
2019-06-14 12:46:50 +0200

    cookiesbar

--- src/main/java/com/szkaminski/backend/repositories/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.szkaminski.backend.repositories;
-
-import com.szkaminski.backend.model.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-}
---
+++ src/main/java/com/szkaminski/frontend/views/CookiesBar.java
@@ -0,0 +1,47 @@
+package com.szkaminski.frontend.views;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.server.VaadinService;
+
+import javax.servlet.http.Cookie;
+import java.util.UUID;
+
+public class CookiesBar {
+
+    public static HorizontalLayout getAcceptCookies() {
+        HorizontalLayout cookiesLayout = new HorizontalLayout();
+        cookiesLayout.add(new Label("Please, accept cookies policy to continue"));
+        Button acceptButton = new Button("Accept");
+        cookiesLayout.add(acceptButton);
+        cookiesLayout.setClassName("cookies-bar");
+
+        if (VaadinService.getCurrentRequest().getCookies()!=null) {
+            if (getCookieByName("AcceptCookie") != null) {
+                cookiesLayout.setVisible(false);
+            } else {
+                cookiesLayout.setVisible(true);
+            }
+        }
+
+        acceptButton.addClickListener(event -> {
+            cookiesLayout.setVisible(false);
+            Cookie acceptCookie = new Cookie("AcceptCookie", UUID.randomUUID().toString());
+            VaadinService.getCurrentResponse().addCookie(acceptCookie);
+        });
+
+        acceptButton.setClassName("cookies-button");
+        return cookiesLayout;
+    }
+
+    private static Cookie getCookieByName(String name) {
+        Cookie[] cookies = VaadinService.getCurrentRequest().getCookies();
+        for (Cookie cookie : cookies) {
+            if (name.equals(cookie.getName())) {
+                return cookie;
+            }
+        }
+        return null;
+    }
+}
