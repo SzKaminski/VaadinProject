b5148c9e2a1a40dd5a6dd97f6d6c232595728b87
Szymon Kamiński <kamin.szymon@gmail.com>
2019-07-10 18:21:16 +0200

    create service

--- src/main/java/com/szkaminski/backend/service/AnaliticsService.java
+++ src/main/java/com/szkaminski/backend/service/AnaliticsService.java
@@ -1,4 +1,33 @@
 package com.szkaminski.backend.service;
 
+
+import com.szkaminski.backend.model.PageAnaliticsSingleton;
+import com.szkaminski.backend.repositories.AnaliticsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class AnaliticsService {
+
+    @Autowired
+    private AnaliticsRepository analiticsRepository;
+
+    public PageAnaliticsSingleton updateAnalitics(){
+        analiticsRepository.save(PageAnaliticsSingleton.getINSTANCE());
+        return PageAnaliticsSingleton.getINSTANCE();
+    }
+
+    public Optional<PageAnaliticsSingleton> getAnalitics(){
+        return analiticsRepository.findById(PageAnaliticsSingleton.getINSTANCE().getId());
+    }
+
+    public int getLikeCounter(){
+        return getAnalitics().get().getLikeCounter();
+    }
+
+    public int getNotLikeCounter(){
+        return getAnalitics().get().getNotLikeCounter();
+    }
 }
